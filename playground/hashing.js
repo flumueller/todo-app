c5587f4570361e8cc922374df6aa3d65342e01ee
Flurin Müller <flurin.mueller@outstander.com>
2017-01-14 19:09:56 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
-var {SHA256} = require('crypto-js');
-var jwt = require('jsonwebtoken');
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	})
+// });
+
+var hashedPassword = '$2a$10$l5huwD1TO8sse/k1O86UeO8lV6wdF/yWaDL9VHtmPA86eb7SRjl46';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+// var data = {
+// 	id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
